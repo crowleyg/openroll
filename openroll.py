52db579758fa9ea9ad06fd2badc30850155da35f
Garrett Crowley <crowleygdev@gmail.com>
2023-10-10 11:31:06 -0700

    dice and result displays now active

--- openroll.py
+++ openroll.py
@@ -1,7 +1,9 @@
+from random import randint
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.core.window import Window
+
 
 # Window Configuration
 Window.size = (400, 600)
@@ -18,24 +20,47 @@ class OpenRollLayout(Widget):
     
     def add_die(self, die):
         self.active_dice[die] += 1
+        self.ids.dice_display.text = self.active_roll()
+
+    def remove_die(self, die):
+        if self.active_dice[die] > 0:
+            self.active_dice[die] -= 1
+            self.ids.dice_display.text = self.active_roll()
+
+    def active_roll(self):
         display_output = 'Rolling: '
         for die, count in self.active_dice.items():
             if count > 0:
                 if display_output != 'Rolling: ':
                     display_output += ' + '
                 display_output += f'{count}{die}'
+        return display_output
+
+    def roll_dice(self):
+        display_output = self.active_roll()
+        total = 0
+        for die, count in self.active_dice.items():
+            if count > 0:
+                display_output += f'\n{die}: ('
+                for i in range(count):
+                    roll_result = randint(1, int(die[1:]))
+                    if i > 0:
+                        display_output += ' + '
+                    display_output += f'{roll_result}'
+                    total += roll_result
+                display_output += ')'
         self.ids.dice_display.text = display_output
 
-    def remove_die(self, die):
-        if self.active_dice[die] > 0:
-            self.active_dice[die] -= 1
-            display_output = 'Rolling: '
-            for die, count in self.active_dice.items():
-                if count > 0:
-                    if display_output != 'Rolling: ':
-                        display_output += ' + '
-                    display_output += f'{count}{die}'
-            self.ids.dice_display.text = display_output
+        if total > 0:
+            self.ids.result_display.text = str(total)
+
+    
+    def clear_dice(self):
+        self.active_dice = {
+            'd4': 0, 'd6': 0, 'd8': 0, 'd10': 0,
+            'd12': 0, 'd20': 0, 'd100': 0}
+        self.ids.dice_display.text = 'Rolling: '
+        self.ids.result_display.text = ''
 
 class OpenRollApp(App):
     def build(self):
